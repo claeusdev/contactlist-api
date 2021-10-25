059c18d6be2e0134ff97b595e79f93889824fb62
Nana Adjei Manu <claeusdev@gmail.com>
2021-10-25 06:06:34 +0000

    add controller action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,4 +3,20 @@ class ContactsController < ApplicationController
     @contacts = Contact.all
     render json: @contacts
   end
+
+  def create
+    contact = Contact.new(contact_params)
+    if contact.save
+      render json: contact, status: :created
+    else
+      render json: contact.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :email, :phone_number)
+  end
 end
