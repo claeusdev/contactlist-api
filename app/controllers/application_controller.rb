9dce2a86fd86c685fc99c318ea935da3f1515776
Nana Adjei Manu <claeusdev@gmail.com>
2021-10-25 06:36:21 +0000

    add exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+
+  def not_destroyed(error)
+    render json: { errors: error.record.errors }, status: :unprocessable_entity
+  end
+
+  def not_found(error)
+    render json: { errors: error }, status: :unprocessable_entity
+  end
 end
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,6 @@
 class ContactsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+
   def index
     @contacts = Contact.all
     render json: @contacts
@@ -15,12 +17,15 @@ class ContactsController < ApplicationController
 
   def destroy
     Contact.find(params[:id]).destroy!
-
     head :no_content
   end
 
 
   private
+
+  def not_destroyed
+    render json: {}, status: :unprocessable_entity
+  end
 
   def contact_params
     params.require(:contact).permit(:first_name, :last_name, :email, :phone_number)
