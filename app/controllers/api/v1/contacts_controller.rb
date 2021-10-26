04ae7b889cef28569f610698308edfc27396fb03
Nana Adjei Manu <claeusdev@gmail.com>
2021-10-26 06:17:38 +0000

    setup api for cors and complete crud

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ContactsController < ApplicationController
   rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+  before_action :set_contact, only: [:show, :update, :destroy]
 
   def index
     @contacts = Contact.all
@@ -7,21 +8,37 @@ class Api::V1::ContactsController < ApplicationController
   end
 
   def create
-    contact = Contact.new(contact_params)
-    if contact.save
-      render json: serialize(contact), status: :created
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      render json: serialize(@contact), status: :created
     else
-      render json: contact.errors, status: :unprocessable_entity
+      render json: @contact.errors, status: :unprocessable_entity
     end
   end
 
+  def update
+    if @contact.update(contact_params)
+      render json: serialize(@contact), status: 200
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: serialize(@contact), status: 200
+  end
+
   def destroy
-    Contact.find(params[:id]).destroy!
+    @contact.destroy!
     head :no_content
   end
 
 
   private
+
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
 
   def serialize(records, options = {})
     ContactSerializer.new(records, options).serializable_hash.to_json
